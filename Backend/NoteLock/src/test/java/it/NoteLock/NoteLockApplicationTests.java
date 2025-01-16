9d8ce04cd7c06ea7cfd571800d2f422e8d5136bf
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-01-16 17:40:37 +0100

    Inizializzazione backend SpringBoot

---
+++ Backend/NoteLock/src/test/java/it/NoteLock/NoteLockApplicationTests.java
@@ -0,0 +1,13 @@
+package it.NoteLock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteLockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
