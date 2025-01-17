b87c35da1eafb54f1713001e0261a335d30ff10b
Giulia Balestra <balestrag9@gmail.com>
2025-01-17 01:13:20 +0100

    Adeguata la classe USER per JPA

--- Backend/NoteLock/src/main/java/it/NoteLock/Models/User.java
+++ Backend/NoteLock/src/main/java/it/NoteLock/Models/User.java
@@ -1,18 +1,51 @@
 package it.NoteLock.Models;
 
-public class User {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import io.jsonwebtoken.lang.Arrays;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity 
+public class User implements UserDetails{
+	@Id
 	private String id;
 	private String nome;
 	private String cognome;
+	private String username;
 	private String email;
 	private String password;
+	@OneToMany(mappedBy = "utente", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Folder> cartelle;
 	
-	public User(String id, String nome, String cognome , String email , String password) {
+	public User(String id, String nome, String cognome ,String username, String email , String password) {
 		this.id = id;
 		this.nome = nome;
 		this.cognome = cognome;
+		this.username = username;
 		this.email = email;
 		this.password = password;
+		this.cartelle = new ArrayList<>();
+	}
+	
+	public List<Folder> getCartelle() {
+		return cartelle;
+	}
+
+	public void setCartelle(List<Folder> cartelle) {
+		this.cartelle = cartelle;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
 	public String getId() {
@@ -53,6 +86,18 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		return Arrays.asList(new SimpleGrantedAuthority("Utente"));
+	}
+
+	@Override
+	public String getUsername() {
+		
+		return username;
 	}
 	
 }
