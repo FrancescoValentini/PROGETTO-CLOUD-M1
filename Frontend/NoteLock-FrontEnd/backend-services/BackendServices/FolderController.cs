c718239aff8b33d0bbeaca57cbb304f0c9842ba9
GiuliaTrz <148765503+GiuliaTrz@users.noreply.github.com>
2025-01-30 18:28:51 +0100

    spostato controller per le cartelle

---
+++ Frontend/NoteLock-FrontEnd/backend-services/BackendServices/FolderController.cs
@@ -0,0 +1,22 @@
+﻿
+using Newtonsoft.Json;
+using System.Text;
+using backend_services.HTTPUtils;
+
+public class FolderController {
+    private String BaseUrl;
+    private String authToken;
+
+    public FolderController(String BaseUrl, String authToken) {
+        this.BaseUrl = BaseUrl;
+        this.authToken = authToken;
+    }
+    
+    public List<FolderDTO> GetFolders() {
+        string json =HTTPUtils.GET(authToken, BaseUrl);
+        return JsonConvert.DeserializeObject<List<FolderDTO>>(json);
+    }
+    
+
+}
+
