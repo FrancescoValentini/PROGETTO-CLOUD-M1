77e36c445a0a5f60bd1fb6ef5b2b34fae82c0e63
Giulia Balestra <balestrag9@gmail.com>
2025-01-30 18:44:38 +0100

    Spostati modelli

--- Frontend/NoteLock-FrontEnd/backend-services/BackendServices/FolderController.cs
+++ Frontend/NoteLock-FrontEnd/backend-services/BackendServices/FolderController.cs
@@ -1,22 +1,29 @@
 ﻿
 using Newtonsoft.Json;
 using System.Text;
-using backend_services.HTTPUtils;
+namespace backend_services.BackendServices
+{
 
-public class FolderController {
-    private String BaseUrl;
-    private String authToken;
 
-    public FolderController(String BaseUrl, String authToken) {
-        this.BaseUrl = BaseUrl;
-        this.authToken = authToken;
+    public class FolderController
+    {
+        private String BaseUrl;
+        private String authToken;
+
+        public FolderController(String BaseUrl, String authToken)
+        {
+            this.BaseUrl = BaseUrl;
+            this.authToken = authToken;
+        }
+
+        public List<FolderDTO> GetFolders()
+        {
+            string json = HTTPUtils.GET(authToken, BaseUrl);
+            return JsonConvert.DeserializeObject<List<FolderDTO>>(json);
+        }
+
+
     }
-    
-    public List<FolderDTO> GetFolders() {
-        string json =HTTPUtils.GET(authToken, BaseUrl);
-        return JsonConvert.DeserializeObject<List<FolderDTO>>(json);
-    }
-    
-
 }
+
 
---
+++ Frontend/NoteLock-FrontEnd/backend-services/Models/FolderDTO.cs
@@ -0,0 +1,7 @@
+﻿
+public class FolderDTO {
+    public string id {  get; set; }
+    public string folderName { get; set; }
+    public string creationTimestamp { get; set; }
+}
+
