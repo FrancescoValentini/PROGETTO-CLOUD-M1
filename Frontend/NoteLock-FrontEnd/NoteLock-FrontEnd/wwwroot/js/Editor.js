8e22752e9cc06abc6254a778a8117ef29b03505e
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-02-27 16:30:58 +0100

    Risolto il bug della XSS presente nella pagina Notes
    Realizzate le funzioni JS per la gestione delle note in modo da non
    dover utilizzare eval e risolvere il problema della XSS

---
+++ Frontend/NoteLock-FrontEnd/NoteLock-FrontEnd/wwwroot/js/Editor.js
@@ -0,0 +1,21 @@
+﻿var simplemde = new SimpleMDE();
+
+function showEditorModal() {
+    new bootstrap.Modal(document.getElementById('EditorModal')).show();
+}
+
+function getTitleText() {
+    return document.getElementById('titleBar').value;
+}
+
+function getNoteText() {
+    return simplemde.value();
+}
+
+function setTitleText(text) {
+    document.getElementById('titleBar').value = text;
+}
+
+function setBodyText(text) {
+    simplemde.value(text);
+}
